0b0b150d3f2662c1cea16d1288b0fae3c2fbcaf6
ultra-hash <jarviscommand3@gmail.com>
2022-03-12 23:45:08 +0530

    PROJECT STARTED

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# Initial Root widget
+root = Tk()
+
+# initial Label
+mylabel = Label(root, text="Hello World")
+
+# showing it on to the screen
+mylabel.pack()
+
+
+
+root.mainloop()
