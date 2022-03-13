d96600fa0eeea36246478835e95f3da12ff991a0
ultra-hash <jarviscommand3@gmail.com>
2022-03-13 14:28:12 +0530

    Added functionalty to add_entries() 	* added  padx and pady space variables for form 	* added confirmation dialog box to popup 	* updated .gitignore

--- functions.py
+++ functions.py
@@ -1,34 +1,62 @@
 from tkinter import *
+from tkinter import messagebox
 
 def add_entries():
+
+    def confirm():
+
+        details_message = f"""
+        Website Name : {Website_name_entry.get()}
+        Username     : {Username_entry.get()}
+        Email        : {Email_entry.get()}
+        Password     : {Password_entry.get()}
+        """
+        # Ask To Confirm
+        confirmation = messagebox.askokcancel(title="Confirm Details", message=details_message)
+        if confirmation:
+            ok = messagebox.showinfo(title="Success", message="Success")
+        
+        
+        # Empty fields
+        Website_name_entry.delete(0, END)
+        Username_entry.delete(0, END)
+        Email_entry.delete(0, END)
+        Password_entry.delete(0, END)
+
     toplevel = Toplevel()
     toplevel.title("ADD Detials")
+    padx_for_form_labels = 10
+    pady_for_form_labels = 2
     
     # main_frame
     add_frame = LabelFrame(toplevel, text="Add Details")
-    add_frame.grid(row=0, column=0)
+    add_frame.grid(row=0, column=0, padx=10, pady=10)
 
-    # Create Form 
+    # Create Form labels
     Website_name = Label(add_frame, text="Website name")
     Username = Label(add_frame, text="Username")
     Email = Label(add_frame, text="Email")
     Password = Label(add_frame, text="Password")
 
-    Website_name.grid(row=0, column=0)
-    Username.grid(row=1, column=0)
-    Email.grid(row=2, column=0)
-    Password.grid(row=3, column=0)
+    Website_name.grid(row=0, column=0, padx=padx_for_form_labels, pady=pady_for_form_labels)
+    Username.grid(row=1, column=0, padx=padx_for_form_labels, pady=pady_for_form_labels)
+    Email.grid(row=2, column=0, padx=padx_for_form_labels, pady=pady_for_form_labels)
+    Password.grid(row=3, column=0, padx=padx_for_form_labels, pady=pady_for_form_labels)
 
-    #Form input fields
+    # Form input fields
     Website_name_entry = Entry(add_frame)
     Username_entry = Entry(add_frame)
     Email_entry = Entry(add_frame)
     Password_entry = Entry(add_frame)
     
-    Website_name_entry.grid(row=0, column=1)
-    Username_entry.grid(row=1, column=1)
-    Email_entry.grid(row=2, column=1)
-    Password_entry.grid(row=3, column=1)
+    Website_name_entry.grid(row=0, column=1, padx=padx_for_form_labels, pady=pady_for_form_labels)
+    Username_entry.grid(row=1, column=1, padx=padx_for_form_labels, pady=pady_for_form_labels)
+    Email_entry.grid(row=2, column=1, padx=padx_for_form_labels, pady=pady_for_form_labels)
+    Password_entry.grid(row=3, column=1, padx=padx_for_form_labels, pady=pady_for_form_labels)
+
+    # confirm bubtton
+    confirm_button = Button(add_frame, text="confirm" , command=confirm)
+    confirm_button.grid(row=4 ,column=0, columnspan=2 , pady=pady_for_form_labels)
 
     toplevel.mainloop()
     
